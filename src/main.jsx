ad3c6154ae3ddde530de9b4f746d6f9a110a1f78
jardadvorak <jaroslav@e-dvorakovi.com>
2024-11-10 22:54:31 +0100

    Modified App and main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { Authenticator } from "@aws-amplify/ui-react";
+// src/main.jsx
+// Entry point of the application
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import '@aws-amplify/ui-react/styles.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
-// technical commit
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { Authenticator } from "@aws-amplify/ui-react";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Authenticator>
+//       <App />
+//     </Authenticator>
+//   </React.StrictMode>
+// );
